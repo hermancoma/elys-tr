6d316d8f9f17056cbf1648674dbb0c17bb31b60e
Herman Coma <herman@gilkor.com>
2018-05-15 16:14:36 +0700

    initial flutter setup

---
+++ elystr/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final webURL = "https://cms-mall-staging.elys.id/";
+    final webView = new MaterialApp(
+      routes: {
+        "/": (_) => new WebviewScaffold(
+              url: webURL,
+              appBar: new AppBar(
+                title: new Text("Elys - TR"),
+              ),
+              withZoom: true,
+              withLocalStorage: true,
+            )
+      },
+    );
+    return webView;
+  }
+}
